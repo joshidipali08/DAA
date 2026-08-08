12c72f3f5651f453cad8fde2c85d198844a94620
Dipali joshi <155424214+joshidipali08@users.noreply.github.com>
2026-08-08 12:23:00 +0530

    Time: 0 ms (100%), Space: 19.3 MB (18.96%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.py
@@ -0,0 +1,11 @@
+class Solution:
+    def removeElement(self, nums, val):
+        k = 0
+
+        for x in nums:
+            if x != val:
+                nums[k] = x
+                k += 1
+
+        return k
+
