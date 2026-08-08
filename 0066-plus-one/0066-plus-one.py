28b9ff84c1604492c731306b141b74b6bda8faf1
Dipali joshi <155424214+joshidipali08@users.noreply.github.com>
2026-08-08 15:22:02 +0530

    Time: 0 ms (100%), Space: 19.2 MB (55.75%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -1,6 +1,6 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-
+ 
         for i in reversed(range(len(digits))):
             if digits[i] != 9:
                 digits[i] += 1
@@ -8,5 +8,7 @@ class Solution:
             else:
                 digits[i] = 0
         return [1] + digits
+        
+        
         
         
